6110af9bfc69ce6cdc8765607c2c8b05f3a313ce
Jenny Quach <jennyquach.99@hotmail.com>
2026-08-29 16:56:08 -0400

    got create rule to work

--- v2/server/api/feed_schedule/router.py
+++ v2/server/api/feed_schedule/router.py
@@ -1,12 +1,13 @@
-from datetime import datetime
+from datetime import datetime, time
 from enum import Enum
+import json
 from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from schema import ScheduleRules
 from db import get_db
 
-router = APIRouter(prefix="/feed_schedule", tags=["feed_schedule"])
 
 class DayOfWeek(str, Enum):
     MON = "monday"
@@ -16,6 +17,9 @@ class DayOfWeek(str, Enum):
     FRI = "friday"
     SAT = "saturday"
     SUN = "sunday"
+
+
+router = APIRouter(prefix="/feed_schedule", tags=["feed_schedule"])
 
 
 @router.get("/")
@@ -31,49 +35,52 @@ def get_schedule(
     db: Session = Depends(get_db),
 ):
     return {
-        "rules": db.query(ScheduleRules).order_by(
-            ScheduleRules.start_time.asc, ScheduleRules.name.asc
-        )
+        "rules": db.query(ScheduleRules)
+        .order_by(ScheduleRules.start_time.asc(), ScheduleRules.name.asc())
+        .all()
     }
+
+
+class RuleCreate(BaseModel):
+    name: str
+    active_days: set[DayOfWeek]
+    start_time: time
+    end_time: time
+    max_educational: Optional[int]
+    min_educational: Optional[int]
+    max_energy: Optional[int]
+    min_energy: Optional[int]
+    categories: list[str]
 
 
 @router.post("/rule")
 def create_rule(
+    payload: RuleCreate,
     db: Session = Depends(get_db),
-    name=str,
-    active_days=set[DayOfWeek],
-    start_time=datetime,
-    end_time=datetime,
-    max_educational=Optional[int],
-    min_educational=Optional[int],
-    max_energy=Optional[int],
-    min_energy=Optional[int],
-    categories=list[str],
 ):
     now = datetime.now()
-
-    ScheduleRules(
-        name=name,
-        monday=DayOfWeek.MON in active_days,
-        tuesday=DayOfWeek.TUE in active_days,
-        wednesday=DayOfWeek.WED in active_days,
-        thursday=DayOfWeek.THU in active_days,
-        friday=DayOfWeek.FRI in active_days,
-        saturday=DayOfWeek.SAT in active_days,
-        sunday=DayOfWeek.SUN in active_days,
-        start_time=start_time,
-        end_time=end_time,
+    rule = ScheduleRules(
+        name=payload.name,
+        monday=DayOfWeek.MON in payload.active_days,
+        tuesday=DayOfWeek.TUE in payload.active_days,
+        wednesday=DayOfWeek.WED in payload.active_days,
+        thursday=DayOfWeek.THU in payload.active_days,
+        friday=DayOfWeek.FRI in payload.active_days,
+        saturday=DayOfWeek.SAT in payload.active_days,
+        sunday=DayOfWeek.SUN in payload.active_days,
+        start_time=payload.start_time.replace(microsecond=0),
+        end_time=payload.end_time.replace(microsecond=0),
         created_at=now,
         updated_at=now,
-        max_educational=max_educational,
-        min_educational=min_educational,
-        max_energy=max_energy,
-        min_energy=min_energy,
-        category_tags=categories
+        max_educational=payload.max_educational,
+        min_educational=payload.min_educational,
+        max_energy=payload.max_energy,
+        min_energy=payload.min_energy,
+        category_tags=json.dumps(payload.categories),
     )
-
-    # TODO:
-    return {}
+    db.add(rule)
+    db.commit()
+    return {"success": True}
 
 
 @router.patch("/rule/{rule_id}/")
