2c8d634f47d57d6b329c8406eab22bf85c43c39a
Jenny Quach <jennyquach.99@hotmail.com>
2026-08-29 17:33:45 -0400

    got update rule to work

--- v2/server/api/feed_schedule/router.py
+++ v2/server/api/feed_schedule/router.py
@@ -46,11 +46,11 @@ class RuleCreate(BaseModel):
     active_days: set[DayOfWeek]
     start_time: time
     end_time: time
-    max_educational: Optional[int]
-    min_educational: Optional[int]
-    max_energy: Optional[int]
-    min_energy: Optional[int]
-    categories: list[str]
+    max_educational: Optional[int] = None
+    min_educational: Optional[int] = None
+    max_energy: Optional[int] = None
+    min_energy: Optional[int] = None
+    categories: Optional[list[str]] = []
 
 
 @router.post("/rule")
@@ -83,13 +83,56 @@ def create_rule(
     return {"success": True}
 
 
+class RuleUpdate(BaseModel):
+    name: Optional[str] = None
+    active_days: Optional[set[DayOfWeek]] = None
+    start_time: Optional[time] = None
+    end_time: Optional[time] = None
+    max_educational: Optional[int] = None
+    min_educational: Optional[int] = None
+    max_energy: Optional[int] = None
+    min_energy: Optional[int] = None
+    categories: Optional[list[str]] = None
+
+
 @router.patch("/rule/{rule_id}/")
 def modify_rule(
     rule_id: str,
+    payload: RuleUpdate,
     db: Session = Depends(get_db),
 ):
-    # TODO:
-    return {}
+    changes = {}
+
+    if payload.name:
+        changes["name"] = payload.name
+    if payload.active_days:
+        changes["monday"] = DayOfWeek.MON in payload.active_days
+        changes["tuesday"] = DayOfWeek.TUE in payload.active_days
+        changes["wednesday"] = DayOfWeek.WED in payload.active_days
+        changes["thursday"] = DayOfWeek.THU in payload.active_days
+        changes["friday"] = DayOfWeek.FRI in payload.active_days
+        changes["saturday"] = DayOfWeek.SAT in payload.active_days
+        changes["sunday"] = DayOfWeek.SUN in payload.active_days
+    if payload.start_time:
+        changes["start_time"] = payload.start_time.replace(microsecond=0)
+    if payload.end_time:
+        changes["end_time"] = payload.end_time.replace(microsecond=0)
+    if payload.max_educational:
+        changes["max_educational"] = payload.max_educational
+    if payload.min_educational:
+        changes["min_educational"] = payload.min_educational
+    if payload.max_energy:
+        changes["max_energy"] = payload.max_energy
+    if payload.min_energy:
+        changes["min_energy"] = payload.min_energy
+    if payload.categories:
+        changes["category_tags"] = json.dumps(payload.categories)
+
+    changes["updated_at"] = datetime.now()
+
+    db.query(ScheduleRules).filter(ScheduleRules.id == rule_id).update(changes)
+    db.commit()
+    return {"success": True, "updated_at": changes["updated_at"]}
 
 
 @router.delete("/rule/{rule_id}/")
@@ -97,5 +140,6 @@ def delete_rule(
     rule_id: str,
     db: Session = Depends(get_db),
 ):
-    # TODO:
-    return {}
+    db.query(ScheduleRules).filter(ScheduleRules.id == rule_id).delete()
+    db.commit()
+    return {"success": True}
